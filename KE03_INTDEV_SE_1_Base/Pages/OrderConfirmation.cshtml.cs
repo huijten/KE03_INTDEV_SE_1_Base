7e3202daf81b79766a057f83ae68303c9b7f201e
Kevin Huijten <1844067huijten@zuyd.nl>
2025-05-15 13:43:20 +0200

    Added Order history

--- KE03_INTDEV_SE_1_Base/Pages/OrderConfirmation.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/OrderConfirmation.cshtml.cs
@@ -1,18 +1,48 @@
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DataAccessLayer.Repositories;
 
 
 namespace KE03_INTDEV_SE_1_Base.Pages;
 
 public class OrderConfirmation : PageModel
 {
+    private readonly PartRepository _partRepository;
+
+    public OrderConfirmation(PartRepository partRepository)
+    {
+        _partRepository = partRepository;
+    }
+
     public void OnGet()
     {
-        
+
     }
-    
+
     public IActionResult OnPostReturnHome()
     {
+        List<int> partIds = TempData["CartPartIds"] as List<int>;
+
+        if (partIds != null)
+        {
+            List<Part> partsInCart = new List<Part>();
+
+            foreach (var k in partIds)
+            {
+                Part part = _partRepository.GetPartById(k);
+                if (part != null)
+                {
+                    partsInCart.Add(part);
+                }
+            }
+        }
+        else
+        {
+            TempData["ErrorMessage"] = "Cart data not found or invalid.";
+            return RedirectToPage("/Cart");
+        }
+
         TempData.Remove("CartPartIds");
         return RedirectToPage("/Index");
     }
---
+++ KE03_INTDEV_SE_1_Base/Pages/OrderHistory.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KE03_INTDEV_SE_1_Base.Pages;
+
+public class order_history : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}

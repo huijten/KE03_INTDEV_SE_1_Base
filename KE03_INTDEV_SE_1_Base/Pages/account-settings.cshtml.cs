de7e160ed730df90a2ce98db9f40e07424bf571c
Kevin Huijten <1844067huijten@zuyd.nl>
2025-06-30 08:52:33 +0200

    Added dummy account-settings

---
+++ KE03_INTDEV_SE_1_Base/Pages/account-settings.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KE03_INTDEV_SE_1_Base.Pages;
+
+public class account_settings : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
